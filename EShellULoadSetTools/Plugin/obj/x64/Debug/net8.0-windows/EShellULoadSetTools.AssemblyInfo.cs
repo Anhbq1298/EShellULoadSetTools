29b1423a813221cfc28674fe641bd01507d2412e
aBui <buiquanganh1298@gmail.com>
2025-11-16 23:49:37 +0700

    a

--- EShellULoadSetTools/Plugin/obj/x64/Debug/net8.0-windows/EShellULoadSetTools.AssemblyInfo.cs
+++ EShellULoadSetTools/Plugin/obj/x64/Debug/net8.0-windows/EShellULoadSetTools.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EShellULoadSetTools")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+648b20245732af0690799dd907f34cc8319781a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08fa5bdb5ea81610e96e6d58ac7d42c55f2d56f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EShellULoadSetTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EShellULoadSetTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
